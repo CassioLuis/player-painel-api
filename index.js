7a59d7f53e485d69e003bd38f4510b453110fd39
CassioLuis <cassiocaruzo@gmail.com>
2023-10-04 09:11:18 -0300

    melhoria logica da porta

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import userRoute from './src/routes/user.route.js'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 const port = 3000
@@ -9,6 +11,6 @@ app.use(cors())
 app.use(express.json())
 app.use('/user', userRoute)
 
-app.listen(port, () => {
-  console.log(`Connection success on port ${port}`)
+app.listen(process.env.PORT || port, () => {
+  console.log(`Connection success on port ${process.env.PORT || port}`)
 })
