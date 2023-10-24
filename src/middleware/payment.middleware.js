a89582fd6056d2b4d1f1ed4da2c99016d471d55f
CassioLuis <cassiocaruzo@gmail.com>
2023-10-23 22:32:08 -0300

    refact payment service

--- src/middleware/payment.middleware.js
+++ src/middleware/payment.middleware.js
@@ -1,17 +1,17 @@
 import Http from '../modules/http.js'
 import Payments from '../services/payments.service.js'
 
-export default class AuthMiddleware {
+export default class PaymentMiddleware {
 
   static async handleWebHookResponse (req, res, next) {
-    const { body: { action, id } } = req
+    const { body: { action, data: { id } } } = req
     if (action === 'payment.created') return res.status(400).json({ message: 'payment action created' })
 
     try {
       const payment = await Http.get(`${process.env.API_MERCADO_PAGO}payments/${id}`)
       if (!payment) return res.status(404).json({ message: 'payment not found' })
 
-      const order = await Payments.getOrderById(payment.id)
+      const order = await Payments.getOrderById(id)
       if (!order) return res.status(404).json({ message: 'order not found' })
       if (order.status === 'approved') return res.status(400).json({ message: 'payment already approved' })
 
