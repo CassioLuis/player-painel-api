dafc2703917dc84ab6970a6bb5ab1fe56fdf2cbc
CassioLuis <cassiocaruzo@gmail.com>
2024-11-02 10:21:07 -0300

    alteracoes

--- src/utils/gold.js
+++ src/utils/gold.js
@@ -1,5 +1,5 @@
 export default class Gold {
   static calc (transactionAmount) {
-    return transactionAmount * 1000 * 100 // 1 real 1000 gold, 100 = quantidade de pratas para totalizar 1 gold
+    return (transactionAmount * 1000 * 100) * 2 // 1 real 1000 gold, 100 = quantidade de pratas para totalizar 1 gold
   }
 }
